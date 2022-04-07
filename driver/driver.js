51041716f39470a2a1a833fb700240e53c211f42
Chattray <chattray.chea@gmail.com>
2022-04-06 18:40:24 -0700

    changed roomId to orders

--- driver/driver.js
+++ driver/driver.js
@@ -1,25 +1,36 @@
-const io = require('socket.io-client');
-const server = io.connect('http://localhost:3000/hub');
+'use strict';
 
-server.on('PICKUP_REQUESTED', (payload) => {
+// const { io } = require('socket.io-client');
+// const server = io.connect('http://localhost:3000/caps');
+
+// const vendorId = 'Gizmos'
+
+// server.emit('join', { roomId: vendorId})
+
+const MessageClient = require('../library/MessageClient.js');
+
+const messageQueue = new MessageClient('orders')
+
+
+messageQueue.subscribe('PICKUP_REQUESTED', (payload) => {
   setTimeout(() => {
     switch(Math.floor(Math.random() * 4)){
       case 1:
-        server.emit('IN_TRANSIT', payload)
+        messageQueue.publish('IN_TRANSIT', payload)
         console.log(`ORDER STATUS: Order for ${payload.payload.orderID} is currently IN TRANSIT.`)
-      case 2:
-        server.emit('DAMAGED', payload)
-        console.log(`ORDER STATUS: Order for ${payload.payload.orderID} has been DAMAGED. PLEASE CONTACT SHIPPER.`)
-      case 3:
-        server.emit('DELAYED', payload)
-        console.log(`ORDER STATUS: Order for ${payload.payload.orderID} has been DELAYED.`)
-    }
-  }, 2000)
-})
-
-server.on('IN_TRANSIT', (payload) => {
-  setTimeout(() => {
-    server.emit('DELIVERED', payload)
-    console.log(`ORDER STATUS: Order for ${payload.payload.orderID} has been DELIVERED.`)
-  }, 2000)
-})
+        case 2:
+          messageQueue.publish('DAMAGED', payload)
+          console.log(`ORDER STATUS: Order for ${payload.payload.orderID} has been DAMAGED. PLEASE CONTACT SHIPPER.`)
+          case 3:
+            messageQueue.publish('DELAYED', payload)
+            console.log(`ORDER STATUS: Order for ${payload.payload.orderID} has been DELAYED.`)
+          }
+        }, 2000)
+      })
+      
+      messageQueue.subscribe('IN_TRANSIT', (payload) => {
+        setTimeout(() => {
+          messageQueue.publish('DELIVERED', payload)
+          console.log(`ORDER STATUS: Order for ${payload.payload.orderID} has been DELIVERED.`)
+        }, 2000)
+      })
